e21955121b342af78d8d2ac8a1ddad57af805fb3
TEAM 27 MACE <96110446+aneettats@users.noreply.github.com>
2022-05-05 23:33:37 +0530

    Add files via upload

---
+++ mailer.py
@@ -0,0 +1,26 @@
+import smtplib, ssl
+
+class Mailer:
+
+    """
+    This script initiaties the email alert function.
+    """
+    def __init__(self):
+        # Enter your email below. This email will be used to send alerts.
+        # E.g., "email@gmail.com"
+        self.EMAIL = "aliazthomaz@gmail.com"
+        # Enter the email password below. Note that the password varies if you have secured
+        self.PASS = "cdmenobyhaedqeid"
+        self.PORT = 465
+        self.server = smtplib.SMTP_SSL('smtp.gmail.com', self.PORT)
+
+    def send(self, mail):
+        self.server = smtplib.SMTP_SSL('smtp.gmail.com', self.PORT)
+        self.server.login(self.EMAIL, self.PASS)
+        # message to be sent
+        SUBJECT = 'ALERT!'
+        TEXT = f'Social distancing violations exceeded!'
+        message = 'Subject: {}\n\n{}'.format(SUBJECT, TEXT)
+        # sending the mail
+        self.server.sendmail(self.EMAIL, mail, message)
+        self.server.quit()
