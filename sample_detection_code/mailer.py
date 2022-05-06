7d97df485464c6c69da1d89cb0e254617866ba1c
1devika79 <52945801+1devika79@users.noreply.github.com>
2022-05-06 23:38:39 +0530

    Update mailer.py

--- sample_detection_code/mailer.py
+++ sample_detection_code/mailer.py
@@ -2,18 +2,9 @@ import smtplib, ssl
 
 class Mailer:
 
-    """
-    This script initiaties the email alert function.
-    """
     def __init__(self):
-        # Enter your email below. This email will be used to send alerts.
-        # E.g., "email@gmail.com"
+   
         self.EMAIL = "aneettathomas2727@gmail.com"
-        # Enter the email password below. Note that the password varies if you have secured
-        # 2 step verification turned on. You can refer the links below and create an application specific password.
-        # Google mail has a guide here: https://myaccount.google.com/lesssecureapps
-        # For 2 step verified accounts: https://support.google.com/accounts/answer/185833
-        # Example: aoiwhdoaldmwopau
         self.PASS = "cdmenobyhaedqeid"
         self.PORT = 465
         self.server = smtplib.SMTP_SSL('smtp.gmail.com', self.PORT)
